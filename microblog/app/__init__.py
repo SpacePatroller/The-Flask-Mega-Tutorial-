13d7fa971911a9692117168a8edc02af65b77ec9
SpacePatroller <emanshupatel@Emanshus-MacBook-Pro.local>
2019-11-24 09:53:47 -0600

    work on chapter3

--- microblog/app/__init__.py
+++ microblog/app/__init__.py
@@ -5,3 +5,5 @@ app = Flask(__name__)
 
 from app import routes
 
+# if __name__ == "__main__":
+#     app.run(debug = True)
--- microblog/app/routes.py
+++ microblog/app/routes.py
@@ -1,8 +1,27 @@
+
+from flask import render_template
 #importing the Flask module from _init_.py
 from app import app
 
 @app.route('/')
+
+@app.route('/home')
+def home():
+    return render_template('base.html')
+    
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    user = {'username': 'Emanshu'}
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        }
+    ]
+    return render_template('index.html',user=user, posts=posts)
+
 
